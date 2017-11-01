c35e76eddfb8e793e7eb3c87419c4fd7f9247cea
Patrick Daly <dalyhabit@gmail.com>
2017-11-01 11:32:14 -0500

    Init

---
+++ server/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var items = require('../database-mysql');
+
+
+var app = express();
+
+app.use(express.static(__dirname + '/../react-client/dist'));
+
+
+app.get('/items', function (req, res) {
+  items.selectAll(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
+
